6fa3309bafa215424ff9818e8c4dee454c7926aa
chibisaravanan <113764635+chibisaravanan@users.noreply.github.com>
2024-01-15 17:59:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,10 +44,10 @@ try:
 
 
 # write your own comment -what does the next line do? 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-#streamlit.dataframe(fruityvice_normalized)
-#streamlit.stop()
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+ write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
